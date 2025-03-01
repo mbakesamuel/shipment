c694096d02eeeb62ba19082ff1253cfc494fc8bd
mbakesamuel <purupela@gmail.com>
2025-03-01 17:04:01 +0100

    latest update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,10 +3,6 @@
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
 import postgres from "postgres";
-import { z } from "zod";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-import bcrypt from "bcryptjs";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -29,50 +25,7 @@ export async function authenticate(
   }
 }
 
-//create user
-/* const FormSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  email: z.string(),
-  password: z.string(),
-}); */
-
-// const CreateUser = FormSchema.omit({ id: true });
-
-/* export type State = {
-  errors?: {
-    name?: string[];
-    email?: string[];
-    password?: string[];
-  };
-  message?: string | null;
-}; */
-
 export async function createUser(formData: FormData) {
-  /*  const validatedFields = CreateUser.safeParse({
-    name: formData.get("name"),
-    email: formData.get("email"),
-    password: formData.get("password"),
-  });
-
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: "Missing Fields. Failed to Create Invoice.",
-    };
-  }
-  const { name, email, password } = validatedFields.data;
-  // Hash the password
-  const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
-
-  await sql`
-      INSERT INTO users (name, email, password)
-      VALUES (${name}, ${email}, ${hashedPassword})
-    `;
-
-  revalidatePath("/dashboard/users");
-  redirect("/dashboard/users"); */
-
   const rawFormData = {
     name: formData.get("name"),
     email: formData.get("email"),
